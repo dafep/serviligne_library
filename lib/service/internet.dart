8503b18e38fadc2cddc9f6631a696ec76c41dea2
Larsanov <ismail.larsanov@epitech.eu>
2020-06-12 10:28:47 +0200

    add stream and create cancel stream function

--- lib/service/internet.dart
+++ lib/service/internet.dart
@@ -1,12 +1,19 @@
+import 'dart:async';
 import "package:connectivity/connectivity.dart";
 
 class Internet {
+  static StreamSubscription internetStream;
+
   static status() {
-    Connectivity().onConnectivityChanged.listen((status) {
+    internetStream = Connectivity().onConnectivityChanged.listen((status) {
       if (status == ConnectivityResult.mobile || status == ConnectivityResult.wifi)
         return true;
       else
         return false;
     });
+  }
+
+  static close() {
+    internetStream.cancel();
   }
 }
