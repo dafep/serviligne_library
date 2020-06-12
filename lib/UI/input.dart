24d6b204dd35be8f4557d5025655abf9d9491de1
Larsanov <ismail.larsanov@epitech.eu>
2020-06-12 14:33:07 +0200

    add input

--- lib/UI/input.dart
+++ lib/UI/input.dart
@@ -1,9 +1,29 @@
 import 'package:flutter/material.dart';
 
 class Input extends StatelessWidget {
+  final double                      width;
+  final double                      height;
+  final TextEditingController       controller;
+  final Color                       backgroundColor;
+
+  Input({
+    this.width,
+    this.height                     = 50,
+    this.controller,
+    this.backgroundColor            = Colors.transparent,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return 
+    return new Container(
+      width: MediaQuery.of(context).size.width,
+      height: height,
+      decoration: BoxDecoration(
+        color: backgroundColor,
+      ),
+      child: new TextField(
+        controller: controller,
+      ),
+    );
   }
 }
